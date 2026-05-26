4d413a06fb8369b33db7b93fde0bcc944d9388cd
JaeGyeom <dale20345@gmail.com>
2026-05-26 16:04:07 +0900

    feat : 곱하기,나누기 추가

--- calculator.py
+++ calculator.py
@@ -4,6 +4,12 @@ def add(a, b):
 def min(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
 print('첫 번째 수를 입력하세요: ')
 a = int(input())
 print('두 번째 수를 입력하세요: ')
@@ -15,5 +21,12 @@ if c == '+':
     print(add(a,b))
 elif c == '-':
     print(min(a,b))
+elif c == '*':
+    print(mul(a,b))
+elif c == '/':
+    if not b:
+        print('0으로 나눌 수 없습니다.')
+    else:
+        print(div(a,b))
 else:
     print('연산자를 올바르게 입력하세요.')
